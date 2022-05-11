b865993781534785723a16254e3a82f06c62ac6a
Daniel Naydenov <daninaydenow@gmail.com>
2022-05-11 15:15:00 +0300

    import messageService

--- heroes-app/src/app/services/hero.service.ts
+++ heroes-app/src/app/services/hero.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+import { MessageService } from './message.service';
+
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
-import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
-  constructor() {}
+  constructor(private messageService: MessageService) {}
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
