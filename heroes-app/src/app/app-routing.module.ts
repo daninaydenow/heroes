c223454409a47bf4f007fce9ea29faa1e28ef838
Daniel Naydenov <daninaydenow@gmail.com>
2022-05-12 10:41:01 +0300

    change order

--- heroes-app/src/app/app-routing.module.ts
+++ heroes-app/src/app/app-routing.module.ts
@@ -5,10 +5,10 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
 
 const routes: Routes = [
-  { path: 'heroes', component: HeroesComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'detail/:id', component: HeroDetailComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'detail/:id', component: HeroDetailComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'heroes', component: HeroesComponent },
 ];
 
 @NgModule({
