38697f5af5c1791d4b9b193823b24efd44d96769
Daniel Naydenov <daninaydenow@gmail.com>
2022-05-12 10:24:55 +0300

    import DI's

--- heroes-app/src/app/components/hero-detail/hero-detail.component.ts
+++ heroes-app/src/app/components/hero-detail/hero-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Hero } from 'src/app/hero';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { HeroService } from 'src/app/services/hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -8,7 +11,11 @@ import { Hero } from 'src/app/hero';
 })
 export class HeroDetailComponent implements OnInit {
   @Input() hero?: Hero;
-  constructor() {}
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private heroService: HeroService
+  ) {}
 
   ngOnInit(): void {}
 }
