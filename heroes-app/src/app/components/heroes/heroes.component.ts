a8c7eda7fac30de5718e5e1bfe6a26b5f835448a
Daniel Naydenov <daninaydenow@gmail.com>
2022-05-12 10:21:58 +0300

    strip dead code

--- heroes-app/src/app/components/heroes/heroes.component.ts
+++ heroes-app/src/app/components/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from 'src/app/hero';
 import { HeroService } from 'src/app/services/hero.service';
-import { MessageService } from 'src/app/services/message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,19 +9,10 @@ import { MessageService } from 'src/app/services/message.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
-  selectedHero?: Hero;
-  constructor(
-    private heroService: HeroService,
-    private messageService: MessageService
-  ) {}
+  constructor(private heroService: HeroService) {}
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
